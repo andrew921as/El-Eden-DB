fee804566e47e60d1ce9bdc7523c7ce6614d9697
andrew <andresfelipecoronado@gmail.com>
2022-08-05 15:30:27 -0500

    Para merge

--- back-eden/dbConnection.js
+++ back-eden/dbConnection.js
@@ -1,9 +1,9 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: 'iron',
+    user: 'Andres',
     host: 'localhost',
     database: 'Eden2',
-    password: 'pg123',
+    password: 'Contralacena2014',
     port: 5432,
 });
 
---
+++ front-eden/src/Functions/SqlFunctions.js
@@ -0,0 +1,69 @@
+//Obtener lista de animales
+function getAnimales() {
+  fetch('http://localhost:3001')
+      .then(response => {
+          return response.text();
+      })
+      .then(data => {
+        console.log("Hola")
+      });
+}
+//Crear animales
+function createAnimal(
+  id_animal,
+  nombre_animal,
+  talla,
+  edad,
+  tipo,
+  motivo_ingreso,
+  observaciones,
+  estado,
+  fecha_ingreso) {
+ /* 
+  let id_animal = prompt('Ingresar codigo identificador')
+  let nombre_animal = prompt('Ingresar nombre')
+  let talla = prompt('Ingresar talla')
+  let edad = prompt('Ingresar edad')
+  let tipo = prompt('Ingresar tipo')
+  let motivo_ingreso = prompt('Ingresar motivo de ingreso')
+  let observaciones = prompt('Ingresar observaciones')
+  let estado = prompt('Ingresar estado')
+  let fecha_ingreso = prompt('Ingresar fecha de ingreso')
+*/
+  fetch(`http://localhost:3001/animales`, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id_animal, nombre_animal, talla, edad, tipo, motivo_ingreso, observaciones, estado, fecha_ingreso }),
+  })
+      .then(response => {
+          return response.text();
+      })
+      .then(data => {
+          alert(data);
+          getAnimales();
+      });
+}
+
+//Borrar un animal
+function deleteAnimal() {
+  let id = prompt('Ingresar codigo animal a borrar');
+  fetch(`http://localhost:3001/animales/${id}`, {
+      method: 'DELETE',
+  })
+      .then(response => {
+          return response.text();
+      })
+      .then(data => {
+          alert(data);
+          getAnimales();
+      });
+}
+
+export {
+  getAnimales,
+  createAnimal,
+  deleteAnimal,
+
+}
