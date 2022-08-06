ec5f124c98eaf5aa7e0d10c7bf1822c83ba87e75
Carlos Cáceres <carlosmicro33@gmail.com>
2022-08-05 21:11:40 -0500

    Responsive + fechas

--- front-eden/src/Pages/RegistrarAnimal.jsx
+++ front-eden/src/Pages/RegistrarAnimal.jsx
@@ -1,9 +1,12 @@
 import { React, useState } from 'react'
 import { Button, Container, Grid, Stack, TextField, Typography, Box } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import { KeyboardDatePicker } from '@material-ui/pickers'
-import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import DateFnsUtils from '@date-io/date-fns';
+
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 
 import { useFormik } from 'formik';
 
@@ -27,41 +30,54 @@ export default function RegistarAnimal() {
       FechaI: '',
       FechaS: '',
       Observaciones: '',
-      Talla:'',
-      Estado:''
+      Talla: '',
+      Estado: ''
     },
     onSubmit: (values) => {
       let animalData = JSON.stringify(values, null, 2)
       alert(animalData);
-      const nombre =JSON.parse(animalData).Nombre
-      const tipo =JSON.parse(animalData).Tipo
-      const talla =JSON.parse(animalData).Talla
-      const edad =JSON.parse(animalData).Edad
-      const motivoIngreso =JSON.parse(animalData).MotivoI
-      const observaciones =JSON.parse(animalData).Observaciones
-      const estado =JSON.parse(animalData).Estado
-      const fechaI =JSON.parse(animalData).FechaI
-      const fechaS =JSON.parse(animalData).FechaS
-      createAnimal('008', nombre, talla,edad,tipo,motivoIngreso,observaciones,estado,fechaI,fechaS);
+      // const nombre = JSON.parse(animalData).Nombre
+      // const tipo = JSON.parse(animalData).Tipo
+      // const talla = JSON.parse(animalData).Talla
+      // const edad = JSON.parse(animalData).Edad
+      // const motivoIngreso = JSON.parse(animalData).MotivoI
+      // const observaciones = JSON.parse(animalData).Observaciones
+      // const estado = JSON.parse(animalData).Estado
+      // const fechaI = JSON.parse(animalData).FechaI
+      // const fechaS = JSON.parse(animalData).FechaS
+      // createAnimal('008', nombre, talla, edad, tipo, motivoIngreso, observaciones, estado, fechaI, fechaS);
     }
   });
+
+  const [valueI, setValueI] = useState(new Date('2014-08-18T21:11:54'));
+
+  const handleChangeI = (newValue) => {
+    setValueI(newValue);
+  };
+
+  const [valueS, setValueS] = useState(new Date('2014-08-18T21:11:54'));
+
+  const handleChangeS = (newValue) => {
+    setValueS(newValue);
+  };
+
 
   return (
     <div className='RegistrarUsuCont'>
       <MenuArriba />
-      <Box sx={{ 
+      <Box sx={{
         width: {
           xs: '100%',
           sm: '100%',
           md: '100%',
           lg: '45%',
           xl: '45%'
-        }, 
-        backgroundColor: 'rgba(226, 226, 226, 0.95)', 
-        paddingTop: 5, 
-        borderRadius: '15px', 
-        flexDirection: "column", 
-        alignContent: "right", 
+        },
+        backgroundColor: 'rgba(226, 226, 226, 0.95)',
+        paddingTop: 5,
+        borderRadius: '15px',
+        flexDirection: "column",
+        alignContent: "right",
         marginTop: {
           xs: 0,
           sm: 0,
@@ -77,147 +93,154 @@ export default function RegistarAnimal() {
           xl: 15
         }
 
-        }}>
-      <Stack
-      spacing={8} 
-      justifyContent="center" 
-      direction={'column'}
-      alignItems={'center'}
-      sx={{paddingBottom:4, paddingTop:3}}>
-        <Typography alignSelf={'center'} variant='h1' color={'#ff5c0c'}>Registrar Animal</Typography>
-        <form onSubmit={formik.handleSubmit}>
-          <Grid container rowSpacing={6}>
-            <Grid item xs={12} md={4}>
-              <Container>
-                <TextField fullWidth id="Nombre" label="Nombre" variant="filled" name='Nombre' value={formik.values.Nombre} onChange={formik.handleChange} />
-              </Container>
+      }}>
+        <Stack
+          spacing={8}
+          justifyContent="center"
+          direction={'column'}
+          alignItems={'center'}
+          sx={{ paddingBottom: 4, paddingTop: 3 }}>
+          <Typography alignSelf={'center'} variant='h1' color={'#ff5c0c'}>Registrar Animal</Typography>
+          <form onSubmit={formik.handleSubmit}>
+            <Grid container rowSpacing={6}>
+              <Grid item xs={12} md={4}>
+                <Container>
+                  <TextField fullWidth id="Nombre" label="Nombre" variant="filled" name='Nombre' value={formik.values.Nombre} onChange={formik.handleChange} />
+                </Container>
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <Container>
+                  <TextField fullWidth id="Tipo" label="Tipo" variant="filled" name='Tipo' value={formik.values.Tipo} onChange={formik.handleChange} />
+                </Container>
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <Container>
+                  <TextField fullWidth id="Edad" label="Edad" variant="filled" name='Edad' value={formik.values.Edad} onChange={formik.handleChange} />
+                </Container>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Container>
+                  <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <DesktopDatePicker
+                      id="Calendario I"
+                      label="Fecha de ingreso"
+                      inputFormat="MM/dd/yyyy"
+                      value={formik.values.FechaI}
+                      onChange={(val) => {
+                        formik.setFieldValue("FechaI", val);
+                      }}
+                      renderInput={(params) =>
+                        <TextField
+                          fullWidth
+                          name='FechaI'
+                          id="FechaI"
+                          label="Fecha Ingreso"
+                          inputVariant="filled"
+                          format="MM/dd/yyyy"
+                          {...params}
+                          />}
+                    />
+                  </LocalizationProvider>
+                </Container>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Container>
+                  <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <DesktopDatePicker
+                      id= "Calendario S"
+                      label="Fecha de salida"
+                      inputFormat="MM/dd/yyyy"
+                      value={formik.values.FechaS}
+                      onChange={(val) => {
+                        formik.setFieldValue("FechaS", val);
+                      }}
+                      renderInput={(params) =>
+                        <TextField
+                          fullWidth
+                          name='FechaS'
+                          id="FechaS"
+                          label="Fecha Salida"
+                          inputVariant="filled"
+                          format="MM/dd/yyyy"
+                          {...params} />}
+                    />
+                  </LocalizationProvider>
+                </Container>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Container>
+                  <TextField fullWidth id="MotivoI" label="Motivo Ingreso" variant="filled" name='MotivoI' value={formik.values.MotivoI} onChange={formik.handleChange} />
+                </Container>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Container>
+                  <TextField sx={{ minHeight: 6 }} fullWidth id="Observaciones" label="Observaciones" variant="filled" name='Observaciones' value={formik.values.Observaciones} onChange={formik.handleChange} />
+                </Container>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Container>
+                  <TextField fullWidth id="Talla" label="Talla" variant="filled" name='Talla' value={formik.values.Talla} onChange={formik.handleChange} />
+                </Container>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Container>
+                  <TextField sx={{ minHeight: 6 }} fullWidth id="Estado" label="Estado" variant="filled" name='Estado' value={formik.values.Estado} onChange={formik.handleChange} />
+                </Container>
+              </Grid>
             </Grid>
-            <Grid item xs={12} md={4}>
+            <Stack direction={'row'} spacing={4} justifyContent={'space-between'} sx={{ paddingTop: 7 }}>
               <Container>
-                <TextField fullWidth id="Tipo" label="Tipo" variant="filled" name='Tipo' value={formik.values.Tipo} onChange={formik.handleChange} />
-              </Container>
-            </Grid>
-            <Grid item xs={12} md={4}>
-              <Container>
-                <TextField fullWidth id="Edad" label="Edad" variant="filled" name='Edad' value={formik.values.Edad} onChange={formik.handleChange} />
-              </Container>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Container>
-                <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                  <KeyboardDatePicker
-                    fullWidth
-                    id="FechaI"
-                    label="Fecha Ingreso"  
-                    inputVariant="filled"
-                    format="MM/dd/yyyy"
-                    value={formik.values.FechaI}
-                    onChange={(val) => {
-                      formik.setFieldValue("FechaI", val);
+                <Button
+                  variant="outlined"
+                  size='medium'
+                  fullWidth
+                  onClick={() => { navigate('/') }}
+                  sx={{ border: '3px solid #881600', borderRadius: 10, ':hover': { border: '3px solid #881600' } }}
+                >
+                  <Typography
+                    sx={{
+                      fontSize: {
+                        xs: 20,
+                        sm: 20,
+                        md: 30,
+                        lg: 30,
+                        xl: 30
+                      }
                     }}
-                   
-                  />
-                </MuiPickersUtilsProvider>
+                    color={'#881600'}
+                  >
+                    Cancelar
+                  </Typography>
+                </Button>
               </Container>
-            </Grid>
-            <Grid item xs={12} md={6}>
               <Container>
-                <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                  <KeyboardDatePicker
-                    fullWidth
-                    id="FechaS"
-                    label="Fecha salida"  
-                    inputVariant="filled"
-                    format="MM/dd/yyyy"
-                    value={formik.values.FechaS}
-                    onChange={(val) => {
-                      formik.setFieldValue("FechaS", val);
+                <Button
+                  sx={{ border: '3px solid #881600', borderRadius: 10, ':hover': { border: '3px solid #881600' } }}
+                  variant='outlined'
+                  size='medium'
+                  fullWidth
+                  type='submit'
+                >
+                  <Typography
+                    sx={{
+                      fontSize: {
+                        xs: 20,
+                        sm: 20,
+                        md: 30,
+                        lg: 30,
+                        xl: 30
+                      }
                     }}
-                  />
-                </MuiPickersUtilsProvider>
+                    color={'#881600'}
+                  >
+                    Guardar
+                  </Typography>
+                </Button>
               </Container>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Container>
-                <TextField fullWidth id="MotivoI" label="Motivo Ingreso" variant="filled" name='MotivoI' value={formik.values.MotivoI} onChange={formik.handleChange} />
-              </Container>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Container>
-                <TextField sx={{ minHeight: 6 }} fullWidth id="Observaciones" label="Observaciones" variant="filled" name='Observaciones' value={formik.values.Observaciones} onChange={formik.handleChange} />
-              </Container>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Container>
-                <TextField fullWidth id="Talla" label="Talla" variant="filled" name='Talla' value={formik.values.Talla} onChange={formik.handleChange} />
-              </Container>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Container>
-                <TextField sx={{ minHeight: 6 }} fullWidth id="Estado" label="Estado" variant="filled" name='Estado' value={formik.values.Estado} onChange={formik.handleChange} />
-              </Container>
-            </Grid>
-          </Grid>
-          <Stack direction={'row'} spacing={4} justifyContent={'space-between'} sx={{ paddingTop: 7 }}>
-            <Container>
-              <Button
-                    variant="outlined"
-                    size='medium'
-                    fullWidth
-                    onClick={() => { navigate('/') }}
-                    sx={{border: '3px solid #881600', borderRadius: 10, ':hover':{border: '3px solid #881600'}}}
-                >
-                <Typography
-                  sx={{
-                    fontSize: {
-                      xs: 20,
-                      sm: 20,
-                      md: 30,
-                      lg: 30,
-                      xl: 30
-                    }
-                  }}
-                  color={'#881600'}
-                >
-                  Salir
-                </Typography>
-              </Button>
-            </Container>
-            <Container>
-              <Button
-                sx={{border:'3px solid #881600', borderRadius: 10, ':hover':{border: '3px solid #881600'}}}
-                variant="outlined"
-                size='medium'
-                fullWidth
-                type='submit'
-              >
-                <Typography
-                  sx={{
-                    fontSize: {
-                      xs: 20,
-                      sm: 20,
-                      md: 30,
-                      lg: 30,
-                      xl: 30
-                    }
-                  }}
-                  color={'#881600'}
-                >
-                  Registrar
-                </Typography>
-              </Button>
-            </Container>
-          </Stack>
-        </form>
-      </Stack>
+            </Stack>
+          </form>
+        </Stack>
       </Box>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
     </div>
   )
 }
--- front-eden/src/Pages/RegistrarVolun.jsx
+++ front-eden/src/Pages/RegistrarVolun.jsx
@@ -28,19 +28,19 @@ export default function RegistrarVolun() {
   return (
     <div className='RegistrarUsuCont'>
       <MenuArriba />
-      <Box sx={{ 
+      <Box sx={{
         width: {
           xs: '100%',
           sm: '100%',
           md: '100%',
           lg: '45%',
           xl: '45%'
-        }, 
-        backgroundColor: 'rgba(226, 226, 226, 0.95)', 
-        paddingTop: 5, 
-        borderRadius: '15px', 
-        flexDirection: "column", 
-        alignContent: "right", 
+        },
+        backgroundColor: 'rgba(226, 226, 226, 0.95)',
+        paddingTop: 5,
+        borderRadius: '15px',
+        flexDirection: "column",
+        alignContent: "right",
         marginTop: {
           xs: 0,
           sm: 0,
@@ -56,90 +56,87 @@ export default function RegistrarVolun() {
           xl: 15
         }
 
-        }}>
-      <Stack spacing={8} justifyContent="center">
-        <Typography alignSelf={'center'} variant='h1' color={'#ff5c0c'}>Registrar Voluntario</Typography>
-        <form onSubmit={formik.handleSubmit}>
-          <Stack
-          spacing={6}
+      }}>
+        <Stack spacing={8}
           justifyContent="center"
-          alignItems="center"
           >
-            <Container>
-              <TextField fullWidth id="Nombre" label="Nombre" variant="filled" name='Nombre' value={formik.values.Nombre} onChange={formik.handleChange} />
-            </Container>
+          <Typography alignSelf={'center'} variant='h1' color={'#ff5c0c'}>Registrar Voluntario</Typography>
+          <form onSubmit={formik.handleSubmit}>
+            <Stack
+              spacing={6}
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Container>
+                <TextField fullWidth id="Nombre" label="Nombre" variant="filled" name='Nombre' value={formik.values.Nombre} onChange={formik.handleChange} />
+              </Container>
 
-            <Container>
-              <TextField fullWidth id="Cedula" label="Cedula" variant="filled" name='Cedula' value={formik.values.Cedula} onChange={formik.handleChange} />
-            </Container>
+              <Container>
+                <TextField fullWidth id="Cedula" label="Cedula" variant="filled" name='Cedula' value={formik.values.Cedula} onChange={formik.handleChange} />
+              </Container>
 
 
-            <Container>
-              <TextField fullWidth id="Cargo" label="Cargo" variant="filled" name='Cargo' value={formik.values.Cargo} onChange={formik.handleChange} />
-            </Container>
+              <Container>
+                <TextField fullWidth id="Cargo" label="Cargo" variant="filled" name='Cargo' value={formik.values.Cargo} onChange={formik.handleChange} />
+              </Container>
 
-            <Container>
-              <TextField fullWidth id="Telefono" label="Télefono" variant="filled" name='Telefono' value={formik.values.Telefono} onChange={formik.handleChange} />
-            </Container>
-          </Stack>
-          <Stack direction={'row'} spacing={4} justifyContent={'space-between'} sx={{ paddingTop: 5, paddingBottom: 5}}>
-            <Container>
-              <Button
-                variant="outlined"
-                size='medium'
-                fullWidth
-                onClick={() => { navigate('/') }}
-                sx={{border: '3px solid #881600', borderRadius: 10, ':hover':{border: '3px solid #881600'}}}
-              >
-                <Typography
-                  sx={{
-                    fontSize: {
-                      xs: 20,
-                      sm: 20,
-                      md: 30,
-                      lg: 30,
-                      xl: 30
-                    }
-                  }}
-                  color={'#ff5c0c'}
+              <Container>
+                <TextField fullWidth id="Telefono" label="Télefono" variant="filled" name='Telefono' value={formik.values.Telefono} onChange={formik.handleChange} />
+              </Container>
+            </Stack>
+            <Stack direction={'row'} spacing={4} justifyContent={'space-between'} sx={{ paddingTop: 5, paddingBottom: 5 }}>
+              <Container>
+                <Button
+                  variant="outlined"
+                  size='medium'
+                  fullWidth
+                  onClick={() => { navigate('/') }}
+                  sx={{ border: '3px solid #881600', borderRadius: 10, ':hover': { border: '3px solid #881600' } }}
                 >
-                  Cancelar
-                </Typography>
-              </Button>
-            </Container>
-            <Container>
-              <Button
-                variant="outlined"
-                size='medium'
-                fullWidth
-                type='submit'
-                sx={{border: '3px solid #881600', borderRadius: 10, ':hover':{border: '3px solid #881600'}}}
-              >
-                <Typography
-                  sx={{
-                    fontSize: {
-                      xs: 20,
-                      sm: 20,
-                      md: 30,
-                      lg: 30,
-                      xl: 30
-                    }
-                  }}
-                  color={'#ff5c0c'}
+                  <Typography
+                    sx={{
+                      fontSize: {
+                        xs: 20,
+                        sm: 20,
+                        md: 30,
+                        lg: 30,
+                        xl: 30
+                      }
+                    }}
+                    color={'#ff5c0c'}
+                  >
+                    Cancelar
+                  </Typography>
+                </Button>
+              </Container>
+              <Container>
+                <Button
+                  variant="outlined"
+                  size='medium'
+                  fullWidth
+                  type='submit'
+                  sx={{ border: '3px solid #881600', borderRadius: 10, ':hover': { border: '3px solid #881600' } }}
                 >
-                  Guardar
-                </Typography>
-              </Button>
-            </Container>
-          </Stack>
-        </form>
-      </Stack>
+                  <Typography
+                    sx={{
+                      fontSize: {
+                        xs: 20,
+                        sm: 20,
+                        md: 30,
+                        lg: 30,
+                        xl: 30
+                      }
+                    }}
+                    color={'#ff5c0c'}
+                  >
+                    Guardar
+                  </Typography>
+                </Button>
+              </Container>
+            </Stack>
+          </form>
+        </Stack>
       </Box>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
     </div>
   )
 }
